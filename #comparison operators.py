82770b35e6f48908f761b5f2e2e8d5380eff006c
beenidiuma <beenidiuma@gmail.com>
2026-09-17 09:36:23 +0530

    Add files via upload

---
+++ #comparison operators.py
@@ -0,0 +1,50 @@
+#comparison operators
+a = 10
+b = 20
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+
+
+#age eligibility checker
+age = int(input("enter your age:"))
+print("Eligible:", age >=18)
+
+#pass or fail checker
+marks = int(input("enter marks:"))
+print("passed:", marks >= 40)
+
+#login validation
+correct_username = "admin"
+correct_password = "1234"
+username = input("enter username:")
+password = input("enter password:")
+print(username == correct_username)
+print(password == correct_password)
+
+
+#logical operators
+age = 25
+citizen = True
+print(age >= 18 and  citizen == True)
+
+
+has_card = False
+has_cash = True
+print(has_card or has_cash)
+
+
+is_logged_in = True
+print(not is_logged_in)
+
+
+#atm eligibility checker
+balance = 10000
+withdraw = 5000
+print(withdraw > 0 and withdraw <= balance)
+
+
+#
