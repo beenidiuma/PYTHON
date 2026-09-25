70ff61d89d82b25dbadc1b40bacd7e574befc357
beenidiuma <beenidiuma@gmail.com>
2026-09-25 09:04:05 +0530

    Add files via upload

---
+++ control_statement.py
@@ -0,0 +1,74 @@
+#problem 1
+a = 15
+b = 20
+if (a>b):
+    print("a is greater than b")
+else:
+    print("a is less than b")    
+
+#problem 2
+A = 20
+B = 30
+if (A>B):
+    print("A is greater than B")
+else:
+    print("A is less than B")
+
+#problem 3
+age = 18
+if(age>= 20):
+    print("eligible for voting")
+else :
+    print("not eligible for voting")
+    
+#problem 4
+age = 15
+if(age>= 18):
+    print("eligible for voting")
+else:
+    print("not eligible for voting")
+
+#problem 5 
+marks = 39
+if(marks>=40):
+    print("pass")      
+else:
+    print("fail")      
+
+
+#problem 6
+number = int(input("enter a number:"))
+if number % 5 == 0:
+    print("divisible by 5")
+
+#problem 7
+number = int(input("enter a number:"))
+if number > 100:
+    print("number is greater than 100")
+else:
+    print("number is less than 100")
+
+#problem 7
+#largest of 2 nubers
+a = int(input("enter a first number:"))
+b = int(input("enter a second number:"))
+if a>b:
+    print("largest:", a)
+elif b > a:
+    print("largest:", b)
+else:
+    print("both are equal")
+
+#problem 8
+#greatest of 3 numbers
+a = int(input("enter a first number:"))
+b = int(input("enter a second number:"))
+c = int(input("enter a third number:"))
+if a>b and a>c:
+    print("greatest:", a)
+elif b>a and b>c:
+    print("greatest:", b)
+else:
+    print("greatest:", c)
+
+    
